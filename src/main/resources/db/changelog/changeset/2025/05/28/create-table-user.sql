423aec25306e1427c239535ebd6da9ab588421dd
angelicium <angeliciumm@gmail.com>
2025-07-14 02:51:24 +0300

    коммит

--- src/main/resources/db/changelog/changeset/2025/05/28/create-table-user.sql
+++ src/main/resources/db/changelog/changeset/2025/05/28/create-table-user.sql
@@ -5,4 +5,8 @@ CREATE TABLE IF NOT EXISTS "user"
     id    UUID         PRIMARY KEY NOT NULL,
     name  VARCHAR(255)             NOT NULL,
     email VARCHAR(100)             NOT NULL
-);
+    );
+INSERT INTO "user" (id, name, email)
+VALUES
+    ('123e4567-e89b-12d3-a456-426614174000', 'NEW', 'Новый тикет, ожидает назначения')
+    ON CONFLICT (id) DO NOTHING;
